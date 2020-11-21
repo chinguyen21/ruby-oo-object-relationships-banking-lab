f5e2ab0198e61c2b512d40018d4324401e955fbb
chinguyen21 <nguyen.thimai@northeastern.edu>
2020-11-21 09:02:58 -0500

    commit tree 57aa3ef02bcd7931202a31be06f0dcf0ddfaf3d1

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,31 @@
 class BankAccount
+  attr_reader :name
+  attr_accessor :balance, :status
+
+  def initialize(name)
+    @name = name
+    @balance = 1000
+    @status = "open"
+  end
+
+  def deposit(money)
+    self.balance += money
+  end
+
+  def display_balance
+    "Your balance is $#{self.balance}."
+  end
+
+  def valid?
+    return true if self.balance > 0 && self.status == "open"
+    false
+  end
+  
+  def close_account
+    self.status = "closed"
+  end
+
+
+
 
 end
